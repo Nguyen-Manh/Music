ae6c9e39975a5c9ad4f5cc67fabd1498723c23d9
VN Meliodasssf <68982254+Nguyen-Manh@users.noreply.github.com>
2021-06-14 09:43:54 +0700

    Update song_list.js

--- song_list.js
+++ song_list.js
@@ -7,34 +7,34 @@ let All_song = [
      singer: "Mạnh đẹp trai [NCS]"
    },
    {
-     name: "Beauz & Jvna - Crazy",
+     name: "song1",
      path: "music/2.mp3",
      img: "images/2.jpg",
-     singer: "NoCopyrightSounds"
+     singer: "singer1"
    },
    {
-     name: "Skydive -Loxbeats",
+     name: "song2",
      path: "music/3.mp3",
      img: "images/3.jpg",
-     singer: "Audio Library"
+     singer: "singer2"
    },
    {
-     name: "Shahed -Indian Fusion",
+     name: "song3",
      path: "music/4.mp3",
      img: "images/4.jpg",
-     singer: "imshahed"
+     singer: "singer3"
    },
    {
-     name: "Syn Cole - Feel Good",
+     name: "song4",
      path: "music/5.mp3",
      img: "images/5.jpg",
-     singer: "NoCopyrightSounds"
+     singer: "singer4"
    },
    {
-    name: "Yusuf Alev - Yoros",
+    name: "song5",
     path: "music/6.mp3",
     img: "images/1.jpg",
-    singer: "Magic Free Release"
+    singer: "singer5"
    }
 ];
 /*you can add more song & images from you computer*/
